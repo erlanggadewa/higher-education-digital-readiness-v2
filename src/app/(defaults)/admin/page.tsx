29d93b572a98399c150e491d024503b69136bfe9
yazid138 <ma39isy@gmail.com>
2024-07-30 12:20:16 +0700

    fixing dashboard and add breadcrumb component

--- src/app/(defaults)/admin/page.tsx
+++ src/app/(defaults)/admin/page.tsx
@@ -1,5 +1,16 @@
-function AdminPage() {
-  return <div>AdminPage</div>;
-}
+import BreadCrumb from '@/components/elements/breadcrumb';
+
+const AdminPage = () => {
+  return (
+    <div>
+      <div className="absolute left-0 top-0 z-[-10] h-36 w-full bg-primary" />
+      <h1 className="mb-2 text-2xl font-bold text-white-light">Dashboard</h1>
+      <BreadCrumb routes={[{ label: 'Dashboard' }]} />
+      <div className="mb-5 w-full rounded-md border border-white-light bg-white p-5 shadow-[4px_6px_10px_-3px_#bfc9d4] dark:border-[#1b2e4b] dark:bg-[#191e3a] dark:shadow-none">
+        <h5 className="text-xl font-semibold text-[#3b3f5c] dark:text-white-light">Selamat datang, Admin</h5>
+      </div>
+    </div>
+  );
+};
 
 export default AdminPage;
