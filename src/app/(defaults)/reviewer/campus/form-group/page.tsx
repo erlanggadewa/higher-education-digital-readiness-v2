26e244fe9db1db7bd0c62269ca289b7ce532dc91
Erlangga Dewa <erlanggadewa7@gmail.com>
2024-08-06 19:44:44 +0700

    refactor: Rename page.tsx to form-group/page.tsx and update imports
    This commit renames the file `page.tsx` to `form-group/page.tsx` in the `src/app/(defaults)/reviewer/campus` directory. It also updates the imports in the file to reflect the new file path. This change improves the organization and clarity of the code structure.

--- src/app/(defaults)/reviewer/campus/form-group/page.tsx
+++ src/app/(defaults)/reviewer/campus/form-group/page.tsx
@@ -1,9 +1,9 @@
 'use client';
+import DataTableReviewerFormGroupCampusComponent from '@/app/(defaults)/reviewer/campus/form-group/data-table';
 import ElementsBreadcrumbsDefault from '@/components/breadcrumbs/elements-breadcrumbs-default';
 import IconDatabase from '@/components/icon/icon-database';
-import DataTableComponent from '@/components/layouts/reviewer/campus/form-group/data-table';
-import TabsFormGroupCampus from '@/components/layouts/reviewer/campus/form-group/tabs';
 import TableSkeletonComponent from '@/components/loading/table-skeleton';
+import TabsFormGroup from '@/components/tabs/tabs-form-group';
 import { generateYearOptions } from '@/utils/date-utils';
 import { Suspense, useState } from 'react';
 import Select from 'react-select';
@@ -31,10 +31,10 @@ function ReviewerFormGroupCampusPage() {
           <span className="badge bg-primary">{year}</span>
         </div>
         <div className="my-3">
-          <TabsFormGroupCampus setSelectedTab={setSelectedTab} />
+          <TabsFormGroup setSelectedTab={setSelectedTab} />
         </div>
         <Suspense fallback={<TableSkeletonComponent />}>
-          <DataTableComponent year={year} selectedTab={selectedTab} />
+          <DataTableReviewerFormGroupCampusComponent year={year} selectedTab={selectedTab} />
         </Suspense>
       </div>
     </div>
