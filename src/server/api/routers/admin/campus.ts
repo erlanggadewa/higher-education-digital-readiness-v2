199e53e79501526de4dd0efdd12b98cd591da2d7
yazid138 <ma39isy@gmail.com>
2024-08-26 16:56:17 +0700

    refactor: hide akun admin

--- src/server/api/routers/admin/campus.ts
+++ src/server/api/routers/admin/campus.ts
@@ -2,6 +2,12 @@ import { createTRPCRouter, protectedProcedure } from '@/server/api/trpc';
 
 export const adminCampusRouter = createTRPCRouter({
   getListCampus: protectedProcedure.query(async ({ ctx }) => {
-    return ctx.db.campusUser.findMany();
+    return ctx.db.campusUser.findMany({
+      where: {
+        NOT: {
+          email: 'admin@email.com',
+        },
+      },
+    });
   }),
 });
