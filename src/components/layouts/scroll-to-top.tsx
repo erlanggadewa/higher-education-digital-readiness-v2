c05787f27cd9921a4edf5f59d2b1c398c015150c
Erlangga Dewa <erlanggadewa7@gmail.com>
2024-09-09 14:41:47 +0700

    Refactor: Improve scroll-to-top button functionality and styling

--- src/components/layouts/scroll-to-top.tsx
+++ src/components/layouts/scroll-to-top.tsx
@@ -1,50 +1,43 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const ScrollToTop = () => {
-    const [showTopButton, setShowTopButton] = useState(false);
+  const [showTopButton, setShowTopButton] = useState(false);
 
-    const goToTop = () => {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
+  const goToTop = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  };
+
+  const onScrollHandler = () => {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      setShowTopButton(true);
+    } else {
+      setShowTopButton(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScrollHandler);
+    return () => {
+      window.removeEventListener('onscroll', onScrollHandler);
     };
+  });
 
-    const onScrollHandler = () => {
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            setShowTopButton(true);
-        } else {
-            setShowTopButton(false);
-        }
-    };
-
-    useEffect(() => {
-        window.addEventListener('scroll', onScrollHandler);
-        return () => {
-            window.removeEventListener('onscroll', onScrollHandler);
-        };
-    });
-
-    return (
-        <div className="fixed bottom-6 z-50 ltr:right-6 rtl:left-6">
-            {showTopButton && (
-                <button type="button" className="btn btn-outline-primary animate-pulse rounded-full bg-[#fafafa] p-2 dark:bg-[#060818] dark:hover:bg-primary" onClick={goToTop}>
-                    <svg width="24" height="24" className="h-4 w-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            opacity="0.5"
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M12 20.75C12.4142 20.75 12.75 20.4142 12.75 20L12.75 10.75L11.25 10.75L11.25 20C11.25 20.4142 11.5858 20.75 12 20.75Z"
-                            fill="currentColor"
-                        ></path>
-                        <path
-                            d="M6.00002 10.75C5.69667 10.75 5.4232 10.5673 5.30711 10.287C5.19103 10.0068 5.25519 9.68417 5.46969 9.46967L11.4697 3.46967C11.6103 3.32902 11.8011 3.25 12 3.25C12.1989 3.25 12.3897 3.32902 12.5304 3.46967L18.5304 9.46967C18.7449 9.68417 18.809 10.0068 18.6929 10.287C18.5768 10.5673 18.3034 10.75 18 10.75L6.00002 10.75Z"
-                            fill="currentColor"
-                        ></path>
-                    </svg>
-                </button>
-            )}
-        </div>
-    );
+  return (
+    <div className="fixed bottom-6 z-50 ltr:right-6 rtl:left-6">
+      {showTopButton && (
+        <button type="button" className="btn btn-outline-primary animate-pulse rounded-full bg-[#fafafa] p-2 dark:bg-[#060818] dark:hover:bg-primary" onClick={goToTop}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+            <path
+              fill="currentColor"
+              d="m3.165 19.503l7.362-16.51c.59-1.324 2.355-1.324 2.946 0l7.362 16.51c.667 1.495-.814 3.047-2.202 2.306l-5.904-3.152c-.459-.245-1-.245-1.458 0l-5.904 3.152c-1.388.74-2.87-.81-2.202-2.306"
+            ></path>
+          </svg>
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default ScrollToTop;
