0585173888966469b52830eb84d572a641b076cc
Erlangga Dewa Sakti <erlanggadewa7@gmail.com>
2025-01-22 21:33:17 +0700

    feat: enhance PrismaClient with optimize and accelerate extensions

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,21 +1,20 @@
 import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
+import { withOptimize } from '@prisma/extension-optimize';
 
 import { env } from '@/env';
 
 const createPrismaClient = () =>
   new PrismaClient({
     log: env.NODE_ENV === 'development' ? ['error', 'warn', 'query'] : ['error'],
-  });
+  })
+    .$extends(withOptimize({ apiKey: env.OPTIMIZE_API_KEY }))
+    .$extends(withAccelerate());
 
 const globalForPrisma = globalThis as unknown as {
   prisma: ReturnType<typeof createPrismaClient> | undefined;
 };
 
-// purePrisma is a PrismaClient instance without any runtime enhancements
-export const purePrisma = createPrismaClient();
-
-// enhance is a function that wraps the PrismaClient instance with zenstack runtime
-// export const db = enhance(globalForPrisma.prisma ?? purePrisma);
-export const db = globalForPrisma.prisma ?? purePrisma;
+export const db = globalForPrisma.prisma ?? createPrismaClient();
 
 if (env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
