8125845dc6f4fae0c135952f34c79f34b22a6919
Erlangga Dewa Sakti <erlanggadewa7@gmail.com>
2025-01-22 21:32:59 +0700

    refactor: replace purePrisma with db in seed script and update schema preview features

--- db/seed.ts
+++ db/seed.ts
@@ -1,35 +1,35 @@
-import { purePrisma } from '@/server/db';
+import { db } from '@/server/db';
 import * as seederData from './seeder';
 
 async function main() {
-  await purePrisma.roleUser.createMany({ data: seederData.roles });
+  await db.roleUser.createMany({ data: seederData.roles });
 
-  await purePrisma.campusUser.createMany({ data: seederData.campusUsers });
-  await purePrisma.campus.createMany({ data: seederData.campuses });
-  await purePrisma.variable.createMany({ data: seederData.variables });
-  await purePrisma.formGroup.createMany({ data: seederData.formGroups });
-  await purePrisma.roleOnFormGroup.createMany({ data: seederData.roleOnFormGroups });
-  await purePrisma.variableOnFormGroup.createMany({
+  await db.campusUser.createMany({ data: seederData.campusUsers });
+  await db.campus.createMany({ data: seederData.campuses });
+  await db.variable.createMany({ data: seederData.variables });
+  await db.formGroup.createMany({ data: seederData.formGroups });
+  await db.roleOnFormGroup.createMany({ data: seederData.roleOnFormGroups });
+  await db.variableOnFormGroup.createMany({
     data: seederData.variableOnFormGroups,
   });
-  await purePrisma.question.createMany({ data: seederData.questions });
-  await purePrisma.option.createMany({ data: seederData.options });
-  await purePrisma.campusAnswer.createMany({ data: seederData.campusAnswers });
-  await purePrisma.levelIndex.createMany({ data: seederData.levelIndices });
-  await purePrisma.levelIndexOnVariables.createMany({
+  await db.question.createMany({ data: seederData.questions });
+  await db.option.createMany({ data: seederData.options });
+  await db.campusAnswer.createMany({ data: seederData.campusAnswers });
+  await db.levelIndex.createMany({ data: seederData.levelIndices });
+  await db.levelIndexOnVariables.createMany({
     data: seederData.levelIndexOnVariables,
   });
-  await purePrisma.campusSurveyLog.createMany({
+  await db.campusSurveyLog.createMany({
     data: seederData.campusSurveyLogs,
   });
 }
 
 main()
   .then(async () => {
-    await purePrisma.$disconnect();
+    await db.$disconnect();
   })
   .catch(async (e) => {
     console.error(e);
-    await purePrisma.$disconnect();
+    await db.$disconnect();
     process.exit(1);
   });
